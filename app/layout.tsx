a108c40736427e21458074cb7b01232b8d867f3f
Sowparna Shome <soupornoshome@gmail.com>
2025-03-09 01:21:54 +0530

    clerk provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Montserrat, Nunito_Sans } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 
 const montserrat = Montserrat({
@@ -26,12 +27,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body
         className={`${nunitoSans.variable} ${montserrat.variable} antialiased`}
       >
         {children}
       </body>
-    </html>
+      </html>
+      </ClerkProvider>
   );
 }
